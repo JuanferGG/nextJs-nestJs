1cad3ce6eb5cd1fcde13fb100b32ed7c1c7785b6
JuanferGG <juanfe15777@gmail.com>
2025-03-28 12:13:57 -0500

    first project practice NextJs, TailwindCSS & framer motion

---
+++ frontend-nextJs/src/sections/ProductShowcase.tsx
@@ -0,0 +1,14 @@
+export const ProductShowcase = () => {
+  return (
+    <section>
+      <div className="container">
+        <div>Boost your productivity</div>
+        <h2>A more effective way to track progress</h2>
+        <p>
+          Effortless trun your ideas into to fully functional, responsive, SaaS
+          website in just minutus with this template.
+        </p>
+      </div>
+    </section>
+  );
+};

2b51c7d5784eaa676b8e858cac99bc6a4e0eaabf
JuanferGG <juanfe15777@gmail.com>
2025-03-30 10:04:11 -0500

    all section complete - pending: framer motion

--- frontend-nextJs/src/sections/Footer.tsx
+++ frontend-nextJs/src/sections/Footer.tsx
@@ -8,10 +8,12 @@ import SocialYoutube from "@/assets/social-youtube.svg";
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
       <div className="container">
-        <Image src={logoSaas} alt="Saas Logo" />
-        <nav>
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:w-full before:blur before:bg-[linear-gradient(to_right,#787BFF,#FB92CF,#FFDD9B,#C2F0B1,#2F8FE)] before:absolute">
+          <Image src={logoSaas} alt="Saas Logo" className="relative" />
+        </div>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6">
           <a href="#">About</a>
           <a href="#">Features</a>
           <a href="#">Customers</a>
@@ -19,13 +21,16 @@ export const Footer = () => {
           <a href="#">Help</a>
           <a href="#">Careers</a>
         </nav>
-        <div>
+        <div className="flex justify-center gap-6 mt-6">
           <SocialX />
           <SocialInsta />
           <SocialLinkedin />
           <SocialPin />
           <SocialYoutube />
         </div>
+        <p className="mt-6">
+          &copy; 2024 Your Company, Inc. All rights reserved.
+        </p>
       </div>
     </footer>
   );

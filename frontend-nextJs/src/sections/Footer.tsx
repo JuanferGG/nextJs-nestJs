303d67ac14b6cf6ef0238d9a608ce2fc28dd3045
JuanferGG <juanfe15777@gmail.com>
2025-03-29 20:45:14 -0500

    new section footer

--- frontend-nextJs/src/sections/Footer.tsx
+++ frontend-nextJs/src/sections/Footer.tsx
@@ -1,3 +1,32 @@
+import Image from "next/image";
+import logoSaas from "@/assets/logosaas.png";
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedin from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer>
+      <div className="container">
+        <Image src={logoSaas} alt="Saas Logo" />
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div>
+          <SocialX />
+          <SocialInsta />
+          <SocialLinkedin />
+          <SocialPin />
+          <SocialYoutube />
+        </div>
+      </div>
+    </footer>
+  );
 };

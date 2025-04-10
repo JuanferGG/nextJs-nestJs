e30299d4a8c405850fdb06823ed716669b0fc92f
JuanferGG <juanfe15777@gmail.com>
2025-04-10 16:58:14 -0500

    command - resources

--- my-first-app-nestJs/src/app.module.ts
+++ my-first-app-nestJs/src/app.module.ts
@@ -3,11 +3,12 @@ import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { PaymentsModule } from './payments/payments.module';
 import { TasksModule } from  './Tasks/Tasks.module'
 
 
 @Module({
-  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],  // ! ---> Modulos en los imports
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule, PaymentsModule],  // ! ---> Modulos en los imports
   controllers: [HelloController],
 })
 export class AppModule {} 

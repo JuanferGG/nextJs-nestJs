5bf18aa481c57a2f5923e36d987301e9d8cd59aa
JuanferGG <juanfe15777@gmail.com>
2025-04-14 15:56:24 -0500

    started portfolio with .ts, f-motion, pages roter

---
+++ multipage-portfolio/src/pages/services/index.tsx
@@ -0,0 +1,45 @@
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+} from "react-icons/rx";
+
+export const servicesData = [
+  {
+    icon: <RxCrop />,
+    title: "Branding",
+    description:
+      "I design and develop websites that are fast, responsive, and user-friendly. I use the latest technologies to ensure that my websites are optimized for search engines and mobile devices.",
+  },
+  {
+    icon: <RxPencil2 />,
+    title: "Desing",
+    description:
+      "I design and develop websites that are fast, responsive, and user-friendly. I use the latest technologies to ensure that my websites are optimized for search engines and mobile devices.",
+  },
+  {
+    icon: <RxDesktop />,
+    title: "Development",
+    description:
+      "I design and develop websites that are fast, responsive, and user-friendly. I use the latest technologies to ensure that my websites are optimized for search engines and mobile devices.",
+  },
+  {
+    icon: <RxReader />,
+    title: "Copywriting",
+    description:
+      "I design and develop websites that are fast, responsive, and user-friendly. I use the latest technologies to ensure that my websites are optimized for search engines and mobile devices.",
+  },
+  {
+    icon: <RxRocket />,
+    title: "SEO",
+    description:
+      "I design and develop websites that are fast, responsive, and user-friendly. I use the latest technologies to ensure that my websites are optimized for search engines and mobile devices.",
+  },
+];
+
+export default function index() {
+  return <div>services</div>;
+}
+

690a44c3dbc22e68ef3e8c26c9ce5c906c072d68
JuanferGG <juanfe15777@gmail.com>
2025-04-19 11:12:20 -0500

    about section start

--- multipage-portfolio/src/pages/about/index.tsx
+++ multipage-portfolio/src/pages/about/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   FaHtml5,
   FaCss3,
@@ -15,76 +17,134 @@ import {
 } from "react-icons/si";
 
 const aboutData = [
-    {
-      title: 'skills',
-      info: [
-        {
-          title: 'Web Development',
-          icons: [
-            <FaHtml5 key={1} />,
-            <FaCss3 key={2} />,
-            <FaJs key={3} />,
-            <FaReact key={4} />,
-            <SiNextdotjs key={5} />,
-            <SiFramer key={6} />,
-            <FaWordpress key={7} />,
-          ],
-        },
-        {
-          title: 'UI/UX Design',
-          icons: [<FaFigma key={1} />, <SiAdobexd key={2} />, <SiAdobephotoshop key={3} />],
-        },
-      ],
-    },
-    {
-      title: 'awards',
-      info: [
-        {
-          title: 'Webby Awards - Honoree',
-          stage: '2011 - 2012',
-        },
-        {
-          title: 'Adobe Design Achievement Awards - Finalist',
-          stage: '2009 - 2010',
-        },
-      ],
-    },
-    {
-      title: 'experience',
-      info: [
-        {
-          title: 'UX/UI Designer - XYZ Company',
-          stage: '2012 - 2023',
-        },
-        {
-          title: 'Web Developer - ABC Agency',
-          stage: '2010 - 2012',
-        },
-        {
-          title: 'Intern - DEF Corporation',
-          stage: '2008 - 2010',
-        },
-      ],
-    },
-    {
-      title: 'credentials',
-      info: [
-        {
-          title: 'Web Development - ABC University, LA, CA',
-          stage: '2011',
-        },
-        {
-          title: 'Computer Science Diploma - AV Technical Institute',
-          stage: '2009',
-        },
-        {
-          title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-          stage: '2006',
-        },
-      ],
-    },
-  ];
+  {
+    title: "skills",
+    info: [
+      {
+        title: "Web Development",
+        icons: [
+          <FaHtml5 key={1} />,
+          <FaCss3 key={2} />,
+          <FaJs key={3} />,
+          <FaReact key={4} />,
+          <SiNextdotjs key={5} />,
+          <SiFramer key={6} />,
+          <FaWordpress key={7} />,
+        ],
+      },
+      {
+        title: "UI/UX Design",
+        icons: [
+          <FaFigma key={1} />,
+          <SiAdobexd key={2} />,
+          <SiAdobephotoshop key={3} />,
+        ],
+      },
+    ],
+  },
+  {
+    title: "awards",
+    info: [
+      {
+        title: "Webby Awards - Honoree",
+        stage: "2011 - 2012",
+      },
+      {
+        title: "Adobe Design Achievement Awards - Finalist",
+        stage: "2009 - 2010",
+      },
+    ],
+  },
+  {
+    title: "experience",
+    info: [
+      {
+        title: "UX/UI Designer - XYZ Company",
+        stage: "2012 - 2023",
+      },
+      {
+        title: "Web Developer - ABC Agency",
+        stage: "2010 - 2012",
+      },
+      {
+        title: "Intern - DEF Corporation",
+        stage: "2008 - 2010",
+      },
+    ],
+  },
+  {
+    title: "credentials",
+    info: [
+      {
+        title: "Web Development - ABC University, LA, CA",
+        stage: "2011",
+      },
+      {
+        title: "Computer Science Diploma - AV Technical Institute",
+        stage: "2009",
+      },
+      {
+        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
+        stage: "2006",
+      },
+    ],
+  },
+];
+
+import Avatar from "@/components/Avatar";
+import Circles from "@/components/Circles";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "@/components/variants";
 
 export default function index() {
-  return <div >About</div>;
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="h-full bg-gray-800/30 py-32 text-center xl-text-left translate-z-10">
+      <Circles />
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px] "
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div className="flex-1 flex flex-col justify-center">Text</div>
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px] ">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4 align-middle">
+            {aboutData.map((item, itemIdex) => {
+              return (
+                <div
+                  key={itemIdex}
+                  className={`${
+                    index === itemIdex &&
+                    "text-red-500 after:w-[100%] after:bg-red-500 after:transition-all after:duration-300"
+                  }
+                  cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] 
+                  after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIdex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className="bg-pink-400/10 py-2xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex}>
+                  {/* Title */}
+                  <div>{item.title}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

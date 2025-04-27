8dbf5f83b51ee32d91d51f50bbd7225b37b506f5
JuanferGG <juanfe15777@gmail.com>
2025-04-27 12:34:09 -0500

    resources, cors & white-list correct

--- todo-app/src/app.module.ts
+++ todo-app/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TaskModule,
+    MongooseModule.forRoot(String(process.env.MONGODB_URI)),
+  ],
 })
 export class AppModule {}

0f6c5113d2ba0d623d9c8ea5f769ccafb6c42e6e
JuanferGG <juanfe15777@gmail.com>
2025-04-28 16:23:14 -0500

    documentacion api swagger

--- todo-app/src/main.ts
+++ todo-app/src/main.ts
@@ -12,6 +12,20 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Todo App - API')
+    .setDescription('The Todo App API description')
+    .setVersion('0.1')
+    .addTag('todo')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
